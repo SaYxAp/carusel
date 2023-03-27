04720df162d6be33339405b18ad1a569c9673748
SaYxAp <119152678+SaYxAp@users.noreply.github.com>
2023-03-27 20:15:15 +0300

    Update prog.py

--- prog.py
+++ prog.py
@@ -4,31 +4,45 @@ app = Flask(__name__)
 
 
 @app.route('/')
-@app.route('/carousel ')
-def carousel ():
+@app.route('/carousel')
+def carousel():
     return """<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <title>Фото животных</title>
-    <link crossorigin="anonymous" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css"
-          integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" rel="stylesheet">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet"
+            integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous">
 </head>
 <body>
 <h3 align="center">Зверушки</h3>
-<div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img src="/static/img/mimi_mishka.jpg" class="d-block w-100" alt="здесь должна была быть картинка, но не нашлась">
+<div class="container">
+        <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+          <img src="/static/img/zaika.jpg" class="d-block w-100" alt="..." width="200" height="1000">
+        </div>
+        <div class="carousel-item">
+          <img src="/static/img/enot.jpg" class="d-block w-100" alt="..." width="200" height="1000">
+        </div>
+        <div class="carousel-item">
+          <img src="/static/img/mimi_mishka.jpg" class="d-block w-100" alt="..." width="200" height="1000">
+        </div>
+      </div>
+      <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+      </button>
+      <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+      </button>
     </div>
-    <div class="carousel-item">
-      <img src="/static/img/enot.jpg" class="d-block w-100" alt="здесь должна была быть картинка, но не нашлась">
-    </div>
-    <div class="carousel-item">
-      <img src="/static/img/zaika.jpg" class="d-block w-100" alt="здесь должна была быть картинка, но не нашлась">
-    </div>
-  </div>
-</div>"""
+</div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/js/bootstrap.bundle.min.js" 
+        integrity="sha384-qKXV1j0HvMUeCBQ+QVp7JcfGl760yU08IQ+GpUo5hlbpg51QRiuqHAJz8+BrxE/N" crossorigin="anonymous"></script>
+</body>
+</html>"""
 
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
